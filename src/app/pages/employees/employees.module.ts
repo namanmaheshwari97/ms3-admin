5da50c414e60d9162b2bcae51a85ff8392c5aa6b
Clemence Yun Chan Lee <yunchan.c.lee@gmail.com>
2017-10-16 14:07:55 -0500

    feat(employees-add): add dependencies & declare components

--- src/app/pages/employees/employees.module.ts
+++ src/app/pages/employees/employees.module.ts
@@ -3,14 +3,18 @@ import {CommonModule} from '@angular/common';
 import {EmployeesComponent} from './employees.component';
 import {EmployeesRoutingModule} from './employees.routing';
 import {Ng2SmartTableModule} from 'ng2-smart-table';
+import {EmployeesTableComponent} from './employees-table/employees-table.component';
+import {EmployeesAddComponent} from './employees-add/employees-add.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     EmployeesRoutingModule,
+    ReactiveFormsModule,
     Ng2SmartTableModule,
   ],
-  declarations: [EmployeesComponent]
+  declarations: [EmployeesComponent, EmployeesTableComponent, EmployeesAddComponent]
 })
 export class EmployeesModule {
 }
--- src/app/pages/employees/employees.routing.ts
+++ src/app/pages/employees/employees.routing.ts
@@ -1,10 +1,22 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {EmployeesComponent} from './employees.component';
+import {EmployeesTableComponent} from './employees-table/employees-table.component';
+import {EmployeesAddComponent} from './employees-add/employees-add.component';
 
 const routes: Routes = [{
   path: '',
   component: EmployeesComponent,
+  children: [
+    {
+      path: '',
+      component: EmployeesTableComponent
+    },
+    {
+      path: 'add',
+      component: EmployeesAddComponent
+    }
+  ]
 }];
 
 @NgModule({
