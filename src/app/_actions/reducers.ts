7cf7376d44cca7a6f56e424b740edf9fe2ab5e44
Clemence Yun Chan Lee <yunchan.c.lee@gmail.com>
2017-10-16 14:35:25 -0500

    feat(employees): add store & action

--- src/app/_actions/reducers.ts
+++ src/app/_actions/reducers.ts
@@ -1,12 +1,15 @@
 import {ActionReducerMap, createSelector} from '@ngrx/store';
 import {Auth} from '../_domains/auth';
 import {Alert} from '../_domains/alert';
+import {EmployeesTable} from '../_domains/employees-table';
 import * as fromAuth from './auth.reducer';
 import * as fromAlert from './alert.reducer';
+import * as fromEmployees from './employees.reducer';
 
 export interface AppState {
   auth: Auth,
-  alert: Alert
+  alert: Alert,
+  employees: EmployeesTable
 }
 
 export const selectAuth = (state: AppState) => state.auth;
@@ -14,7 +17,11 @@ export const selectAuthToken = createSelector(selectAuth, (state: Auth) => state
 
 export const selectAlert = (state: AppState) => state.alert;
 
+export const selectEmployees = (state: AppState) => state.employees;
+export const selectEmployeesList = createSelector(selectEmployees, (state: EmployeesTable) => state.list);
+
 export const reducers: ActionReducerMap<AppState> = {
   auth: fromAuth.reducer,
-  alert: fromAlert.reducer
+  alert: fromAlert.reducer,
+  employees: fromEmployees.reducer
 };
