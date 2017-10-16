9763517c862415d3383575acb1a2054a5f5c33e9
Clemence Yun Chan Lee <yunchan.c.lee@gmail.com>
2017-10-16 13:40:45 -0500

    feat(employees-table): add employee table

--- src/app/pages/employees/employees.component.ts
+++ src/app/pages/employees/employees.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-employees',
@@ -6,10 +6,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employees.component.scss']
 })
 export class EmployeesComponent implements OnInit {
+  settings = {
+    attr: {
+      class: 'table'
+    },
+    actions: {
+      edit: false
+    },
+    hideSubHeader: true,
+    columns: {
+      id: {
+        title: 'ID',
+        sort: false,
+        filter: false
+      },
+      email: {
+        title: 'Email',
+        sort: false,
+        filter: false
+      },
+      phoneNumber: {
+        title: 'Phone Number',
+        sort: false,
+        filter: false
+      }
+    }
+  };
 
-  constructor() { }
+  data = [
+    {
+      id: 1,
+      email: 'Sincere@april.biz',
+      phoneNumber: '608-618-8342'
+    },
+    {
+      id: 2,
+      email: 'Shanna@melissa.tv',
+      phoneNumber: '608-618-8343'
+    }
+  ];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
+
 
 }
--- src/app/pages/employees/employees.module.ts
+++ src/app/pages/employees/employees.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {EmployeesComponent} from './employees.component';
 import {EmployeesRoutingModule} from './employees.routing';
+import {Ng2SmartTableModule} from 'ng2-smart-table';
 
 @NgModule({
   imports: [
     CommonModule,
-    EmployeesRoutingModule
+    EmployeesRoutingModule,
+    Ng2SmartTableModule,
   ],
   declarations: [EmployeesComponent]
 })
