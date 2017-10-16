942c3d6f27d473c52bb54ba5f6f2458a2cb50f66
Clemence Yun Chan Lee <yunchan.c.lee@gmail.com>
2017-10-15 23:17:18 -0500

    lib(clarity-ui): add clarity-ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {ClarityModule} from 'clarity-angular';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ClarityModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
