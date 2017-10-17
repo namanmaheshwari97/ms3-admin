90130ff932daf006c3819ae7cabf0d44d2bccd81
Clemence Yun Chan Lee <yunchan.c.lee@gmail.com>
2017-10-16 21:52:15 -0500

    feat(blacklist.effects): add effects

---
+++ src/app/pages/email-blacklist/email-blacklist-add.effects.ts
@@ -0,0 +1,67 @@
+import {Injectable} from '@angular/core';
+import {Action} from '@ngrx/store';
+import {Actions, Effect} from '@ngrx/effects';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/observable/of';
+
+import {API} from '../../core/api-endpoints.constant';
+
+import {RestApiService} from '../../core/rest-api.service';
+import {RestApiRequest} from '../../core/rest-api-request';
+import {EmailItem} from "../../_domains/email-item";
+import * as EmailBlacklistActions from '../../_actions/email-blacklist.actions';
+import * as AlertActions from '../../_actions/alert.actions';
+import {EmailForm} from '../../_domains/email-form';
+
+export namespace EmailBlacklistAdd {
+  export const REQUEST = 'EmailBlacklistAddEffects.REQUEST';
+  export const SUCCESS = 'EmailBlacklistAddEffects.SUCCESS';
+  export const ERROR = 'EmailBlacklistAddEffects.ERROR';
+
+  export class Request implements Action {
+    readonly type = REQUEST;
+
+    constructor(public payload: EmailForm) {
+    }
+  }
+
+  export class Success implements Action {
+    readonly type = SUCCESS;
+
+    constructor(public payload: EmailItem) {
+    }
+  }
+
+  export class Error implements Action {
+    readonly type = ERROR;
+  }
+
+  @Injectable()
+  export class Effects {
+    @Effect() onRequest$: Observable<Action> = this.actions$
+      .ofType(REQUEST)
+      .map((action: Request) => action.payload)
+      .switchMap((payload) => {
+        let request = new RestApiRequest(API.BLACKLIST.CREATE);
+        request.setBody(payload);
+
+        return this._api.send(request)
+          .map(response => new Success(response))
+          .catch(error => Observable.of(new Error()));
+      });
+
+    @Effect() onSuccess$: Observable<Action> = this.actions$
+      .ofType(SUCCESS)
+      .map((action: Success) => action.payload)
+      .map((payload) => new EmailBlacklistActions.AddItem(payload));
+
+    @Effect() onError$: Observable<Action> = this.actions$
+      .ofType(ERROR)
+      .map(() => new AlertActions.SetError('Something Went Wrong'));
+
+    constructor(private _api: RestApiService, private actions$: Actions) {
+    }
+  }
+}
