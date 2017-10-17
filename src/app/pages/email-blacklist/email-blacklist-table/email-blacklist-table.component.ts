ad94a7b5ad8807a331ab017a297c7ae45552b5dd
Clemence Yun Chan Lee <yunchan.c.lee@gmail.com>
2017-10-16 21:57:05 -0500

    feat(blacklist-table): add get/delete

--- src/app/pages/email-blacklist/email-blacklist-table/email-blacklist-table.component.ts
+++ src/app/pages/email-blacklist/email-blacklist-table/email-blacklist-table.component.ts
@@ -1,4 +1,9 @@
 import {Component, OnInit} from '@angular/core';
+import {Store} from '@ngrx/store';
+import * as fromRoot from '../../../_actions/reducers';
+import {EmailItem} from '../../../_domains/email-item';
+import {EmailBlacklist} from '../email-blacklist.effects';
+import {EmailBlacklistRemove} from '../email-blacklist-remove.effects';
 
 @Component({
   selector: 'app-email-blacklist-table',
@@ -29,27 +34,19 @@ export class EmailBlacklistTableComponent implements OnInit {
     }
   };
 
-  data = [
-    {
-      id: 1,
-      email: 'black1@email.com'
-    },
-    {
-      id: 2,
-      email: 'black2@email.com'
-    },
-    {
-      id: 3,
-      email: 'black3@email.com'
-    },
-  ];
+  data: EmailItem[];
 
-  constructor() {
+  constructor(private _store: Store<any>) {
+    this._store.select(fromRoot.selectBlacklistList).subscribe((list) => {
+      this.data = list;
+    });
   }
 
   ngOnInit() {
+    this._store.dispatch(new EmailBlacklist.Request());
   }
 
   onDelete(event) {
+    this._store.dispatch(new EmailBlacklistRemove.Request(event.data.id));
   }
 }
