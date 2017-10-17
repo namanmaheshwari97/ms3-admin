8075bddeda35ce9e8fe53ee037406fc57736e4c7
Clemence Yun Chan Lee <yunchan.c.lee@gmail.com>
2017-10-16 21:09:59 -0500

    feat(email-blacklist-table): add component

---
+++ src/app/pages/email-blacklist/email-blacklist-table/email-blacklist-table.component.ts
@@ -0,0 +1,54 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-email-blacklist-table',
+  templateUrl: './email-blacklist-table.component.html',
+  styleUrls: ['./email-blacklist-table.component.scss']
+})
+export class EmailBlacklistTableComponent implements OnInit {
+  settings = {
+    attr: {
+      class: 'table'
+    },
+    actions: {
+      edit: false
+    },
+    mode: 'external',
+    columns: {
+      id: {
+        title: 'ID',
+        sort: false,
+        filter: false
+      },
+      email: {
+        title: 'Email',
+        sort: false,
+        filter: false
+      }
+    }
+  };
+
+  data = [
+    {
+      id: 1,
+      email: 'black1@email.com'
+    },
+    {
+      id: 2,
+      email: 'black2@email.com'
+    },
+    {
+      id: 3,
+      email: 'black3@email.com'
+    },
+  ];
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  onDelete(event) {
+  }
+}
