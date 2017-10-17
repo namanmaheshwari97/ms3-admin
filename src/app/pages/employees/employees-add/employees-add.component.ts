70cf96d429a15a64fc3ca5a13292e51dfdfdc9bb
Clemence Yun Chan Lee <yunchan.c.lee@gmail.com>
2017-10-16 19:29:00 -0500

    feat(employees-form): add create method

--- src/app/pages/employees/employees-add/employees-add.component.ts
+++ src/app/pages/employees/employees-add/employees-add.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {EmployeesAdd} from '../employees-add.effects';
+import {Store} from '@ngrx/store';
 
 @Component({
   selector: 'app-employees-add',
@@ -9,24 +11,20 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 export class EmployeesAddComponent implements OnInit {
   employeeForm: FormGroup;
 
-  constructor() {
-    this.employeeForm = new FormGroup({
-      email: new FormControl('', Validators.required),
-      phoneNumber: new FormControl(''),
-      password: new FormControl('', Validators.required)
+  constructor(private _store: Store<any>, private _fb: FormBuilder) {
+
+    this.employeeForm = this._fb.group({
+      email: ['', Validators.required],
+      phoneNumber: [''],
+      password: ['', Validators.required]
     });
   }
 
   ngOnInit() {
   }
 
-
-  submitted = false;
   onSubmit() {
-  }
 
-  addNewEmployeeAddress() {
-    this.employeeForm.reset();
-    this.submitted = false;
+    this._store.dispatch(new EmployeesAdd.Request(this.employeeForm.value))
   }
 }
