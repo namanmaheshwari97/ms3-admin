ef46ae90d49da0b83eb8b46d1334e317e9c6bfbf
Clemence Yun Chan Lee <yunchan.c.lee@gmail.com>
2017-10-28 13:01:46 -0500

    fix(header): add missing header features

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {PAGES_MENU_ITEMS} from './pages-menu.constant';
+import {Store} from '@ngrx/store';
+import * as fromRoot from '../_actions/reducers';
+import {Auth} from '../_domains/auth';
+import {SignOut} from '../auth/sign-out.effects';
 
 @Component({
   selector: 'app-pages',
@@ -8,10 +12,18 @@ import {PAGES_MENU_ITEMS} from './pages-menu.constant';
 })
 export class PagesComponent implements OnInit {
   public menu = PAGES_MENU_ITEMS;
+  email: string;
 
-  constructor() { }
+  constructor(private _store: Store<any>) {
+
+    this._store.select(fromRoot.selectAuth).subscribe((auth: Auth) => this.email = auth.email);
+  }
 
   ngOnInit() {
   }
 
+  onSignOut() {
+
+    this._store.dispatch(new SignOut.Request());
+  }
 }
