8d41e798261aad102764bf7a527fe08b06e37813
Clemence Yun Chan Lee <yunchan.c.lee@gmail.com>
2017-11-21 19:45:24 -0600

    spec(properties-table): fix test

--- src/app/pages/properties/properties-table/properties-table.component.spec.ts
+++ src/app/pages/properties/properties-table/properties-table.component.spec.ts
@@ -1,16 +1,29 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { PropertiesTableComponent } from './properties-table.component';
+import {PropertiesTableComponent} from './properties-table.component';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+import {Store} from '@ngrx/store';
 
 describe('PropertiesTableComponent', () => {
   let component: PropertiesTableComponent;
   let fixture: ComponentFixture<PropertiesTableComponent>;
 
+  const mockStore = {
+    select: jasmine.createSpy('select').and.returnValue(Observable.of([])),
+    dispatch: jasmine.createSpy('dispatch')
+  };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PropertiesTableComponent ]
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        {provide: Store, useValue: mockStore}
+      ],
+      declarations: [PropertiesTableComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
